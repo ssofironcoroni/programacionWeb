c4e2a92a13e821b555417a026325bb4b4cbb8f7a
ssofironcoroni <sroncoroni06@gmail.com>
2023-06-22 18:21:41 -0300

    ENTREGA final

--- consultas.js
+++ consultas.js
@@ -20,11 +20,11 @@ function reservarConsulta()
     return item.destino === viaje.destino;
   });
 
-  if (hayReserva) {
-    
+  if (hayReserva) { 
     hayReserva.cantReservas++;
-  } else {
-    
+  } 
+  
+  else {
     carroDeConsultas.push(viaje);
   }
 
@@ -33,15 +33,20 @@ function reservarConsulta()
 
 function consultasUpdates() {
   var carroCon = document.getElementById('carroCon');
+  
   var totalConsulta = document.getElementById('totalConsulta');
+  
   var eliminarConsulta = document.getElementById('eliminarConsulta');
   carroCon.innerHTML = '';
+  
   totalConsulta.textContent = '$';
 
 
   for (var m = 0; m < carroDeConsultas.length; m++) {
     var viaje = carroDeConsultas[m];
+   
     var precioF = viaje.costoConsulta * viaje.cantReservas;
+   
     var orden = document.createElement('tr');   /* tr crea en forma de tabla */
     orden.innerHTML = `<td>${viaje.destino}</td><td>$${viaje.costoConsulta}</td><td>${viaje.cantReservas}</td><td>$${precioF}</td><td><button class="eliminarConsulta btnBorrar" data-elim="${m}">Eliminar</button></td>`;
     carroCon.appendChild(orden);
@@ -55,22 +60,15 @@ function consultasUpdates() {
   eliminarConsulta.addEventListener('click', vaciarCarroConsultas);
   var botonesBorrarConsulta = document.getElementsByClassName('eliminarConsulta');
   
-
-  
   for (var n = 0; n < botonesBorrarConsulta.length; n++) {
     botonesBorrarConsulta[n].addEventListener('click', borrarUnaConsulta);
   }
 }
 
-
 function vaciarCarroConsultas() {
   carroDeConsultas = [];
   consultasUpdates();
 }
-
-
-
-
 function borrarUnaConsulta() {
   var elim = parseInt(this.dataset.elim);
   carroDeConsultas.splice(elim, 1);
