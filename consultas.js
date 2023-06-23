bbd5aaa84faca55507630c0db6696535c6f8f500
ssofironcoroni <sroncoroni06@gmail.com>
2023-06-23 13:40:08 -0300

    ENTREGA FINAL

--- consultas.js
+++ consultas.js
@@ -10,6 +10,8 @@ for (var i = 0; i < botonesReservar.length; i++) {
   botonesReservar[i].addEventListener('click', reservarConsulta);
   }
 
+
+  /* los elementos que vamos a agregar de cada consulta */
 function reservarConsulta() 
 {
     var viaje = {
@@ -18,6 +20,8 @@ function reservarConsulta()
       cantReservas: 1
   };
 
+
+  /* si es que está en el carrito ... */
   var hayReserva = carroDeConsultas.find(function(item) {
     return item.destino === viaje.destino;
   });
@@ -33,14 +37,13 @@ else {
 }
 
 function consultasUpdates() {
-  var carroCon = document.getElementById('carroCon');
+  let carroCon = document.getElementById('carroCon');
   
-  var totalConsulta = document.getElementById('totalConsulta');
+  let totalConsulta = document.getElementById('totalConsulta');
   
-  var eliminarConsulta = document.getElementById('eliminarConsulta');
-  carroCon.innerHTML = '';
+  let eliminarConsulta = document.getElementById('eliminarConsulta');
   
-  totalConsulta.textContent = '$';
+  carroCon.innerHTML = '';  totalConsulta.textContent = '$';
 
 
   for (var m = 0; m < carroDeConsultas.length; m++) {
@@ -48,31 +51,34 @@ function consultasUpdates() {
    
     var precioF = viaje.costoConsulta * viaje.cantReservas;
    
-    var orden = document.createElement('tr');   /* tr crea en forma de tabla */
+    var orden = document.createElement('tr');   /* recordar: que tr crea en forma de tabla */
     orden.innerHTML = `<td>${viaje.destino}</td><td>$${viaje.costoConsulta}</td><td>${viaje.cantReservas}</td><td>$${precioF}</td><td>
     <button class="eliminarConsulta btnBorrar" data-elim="${m}"> <img class="tachoCarrito" src="./images/tacho.png"> </button></td>`;
     carroCon.appendChild(orden);
   }
 
   var precioFinal = carroDeConsultas.reduce(function(x, viaje) {
-    return x + viaje.costoConsulta * viaje.cantReservas;
-  }, 0);
+    return x + viaje.costoConsulta * viaje.cantReservas;   }, 0);
 
-  totalConsulta.textContent='$'+ precioFinal.toFixed(2);
-  eliminarConsulta.addEventListener('click', vaciarCarroConsultas);
-  var botonesBorrarConsulta = document.getElementsByClassName('eliminarConsulta');
+  totalConsulta.textContent='$'+ precioFinal.toFixed(2); eliminarConsulta.addEventListener('click', vaciarCarroConsultas);
+  
+  let botonesBorrarConsulta = document.getElementsByClassName('eliminarConsulta');
   
   for (var n = 0; n < botonesBorrarConsulta.length; n++) {
     botonesBorrarConsulta[n].addEventListener('click', borrarUnaConsulta);
   }
 }
-
+/* vaciamos el carro y lo reiniciamos */
 function vaciarCarroConsultas() {
-  carroDeConsultas = [];
-  consultasUpdates();
+  carroDeConsultas = [];   /* eliminamos lo que haya en carrito y reiniciamos el array */
+  consultasUpdates();      /* lo actualizamos */
 }
+
+/* borramos del array de carrito y actualizamos */
 function borrarUnaConsulta() {
   var elim = parseInt(this.dataset.elim);
   carroDeConsultas.splice(elim, 1);
   consultasUpdates();
 }
+
+
